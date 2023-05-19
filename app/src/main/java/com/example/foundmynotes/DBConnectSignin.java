1d44a46d84a51d69d23717560127dbb51bc9ae2b
JohnChristianIdul <idulchristian@gmail.com>
2023-05-19 13:38:25 +0800

    Added a connect to DB SQLite and functionality to Sign Up

---
+++ app/src/main/java/com/example/foundmynotes/DBConnectSignin.java
@@ -0,0 +1,81 @@
+package com.example.foundmynotes;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBConnectSignin extends SQLiteOpenHelper {
+
+    public DBConnectSignin(Context context) {
+        super(context, "user_data.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("create Table user(email_address TEXT primary key, password TEXT, Name TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("drop Table if exists user");
+    }
+
+    public boolean insertUserData(String email_address, String password, String name){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("email_address", email_address);
+        contentValues.put("password", password);
+        contentValues.put("name", name);
+        long result = DB.insert("user", null, contentValues);
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+
+// can be use for update profile
+//    public boolean updateUserData(String email_address, String password, String name){
+//        SQLiteDatabase DB = this.getWritableDatabase();
+//        ContentValues contentValues = new ContentValues();
+//        contentValues.put("email_address", email_address);
+//        contentValues.put("password", password);
+//        contentValues.put("name", name);
+//        Cursor cursor = DB.rawQuery("select * from user where name = ?", new String[] {name});
+//        if(cursor.getCount()>0) {
+//            long result = DB.update("user", contentValues, "name=?", new String[] {name});
+//            if(result == -1)
+//                return false;
+//            else
+//                return true;
+//        }else {
+//            return false;
+//        }
+//    }
+
+//can be used for delete account
+//    public boolean deleteUserData(String email_address, String password, String name){
+//        SQLiteDatabase DB = this.getWritableDatabase();
+//        Cursor cursor = DB.rawQuery("select * from user where name = ?", new String[] {name});
+//        if(cursor.getCount()>0) {
+//            long result = DB.delete("user", "name=?", new String[] {name});
+//            if(result == -1)
+//                return false;
+//            else
+//                return true;
+//        }else {
+//            return false;
+//        }
+//    }
+
+    //can be used for profile
+    public Boolean getUserData(String email, String password){
+        SQLiteDatabase DB = this.getWritableDatabase();
+        Cursor cursor = DB.rawQuery("select * from user where email_address=? and password =?", new String[] {email, password});
+        if(cursor.getCount() > 0)
+            return true;
+        else
+            return false;
+    }
+}
--- app/src/main/java/com/example/foundmynotes/Signup.java
+++ app/src/main/java/com/example/foundmynotes/Signup.java
@@ -2,13 +2,58 @@ package com.example.foundmynotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Signup extends AppCompatActivity {
+    EditText name, email, password, confirm_password;
+    TextView Login;
+    Button SignUp;
+    DBConnectSignin DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        name = findViewById(R.id.tfname);
+        email = findViewById(R.id.tfemail_address);
+        password = findViewById(R.id.tfpassword);
+        confirm_password = findViewById(R.id.tfconfirmpassword);
+
+        SignUp = findViewById(R.id.btnSignUp);
+        Login = findViewById(R.id.tfLogin);
+        DB = new DBConnectSignin(this);
+
+        SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name_text = name.getText().toString();
+                String email_text = email.getText().toString();
+                String password_text = password.getText().toString();
+                String confirm_password_text = confirm_password.getText().toString();
+
+                if(!password.equals(confirm_password_text)){
+                    Toast.makeText(Signup.this, "Password do not match", Toast.LENGTH_SHORT).show();
+                    confirm_password.requestFocus();
+                    return;
+                }
+
+                boolean checkInserted = DB.insertUserData(name_text, email_text, password_text);
+                if(checkInserted){
+                    Toast.makeText(Signup.this, "Successful", Toast.LENGTH_SHORT).show();
+                    Intent login = new Intent(getApplicationContext(), Login.class);
+                    startActivity(login);
+                }else{
+                    Toast.makeText(Signup.this, "Check your input", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
