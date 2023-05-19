00cbee978425efc372265c720f51e66141a38e25
VennD0 <120389311+VennD0@users.noreply.github.com>
2023-05-19 14:06:24 +0800

    lostfoundreport

---
+++ app/src/main/java/com/example/foundmynotes/lostfoundreport.java
@@ -0,0 +1,31 @@
+package com.example.foundmynotes;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class lostfoundreport extends AppCompatActivity {
+
+    Button btngetstarted;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btngetstarted= findViewById(R.id.btngetstarted);
+
+        btngetstarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent login = new Intent(getApplicationContext(), Login.class);
+                startActivity(login);
+            }
+        });
+
+    }
+}
