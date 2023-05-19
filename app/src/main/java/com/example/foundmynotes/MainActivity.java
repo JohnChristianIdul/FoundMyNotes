780e1204f86a6e0d694aa3afca66ce8f73847764
JohnChristianIdul <idulchristian@gmail.com>
2023-05-19 11:30:07 +0800

    Initial commit

---
+++ app/src/main/java/com/example/foundmynotes/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.foundmynotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btngetstarted;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btngetstarted= findViewById(R.id.btngetstarted);
+
+        btngetstarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent login = new Intent(getApplicationContext(), Login.class);
+                startActivity(login);
+            }
+        });
+
+    }
+}
