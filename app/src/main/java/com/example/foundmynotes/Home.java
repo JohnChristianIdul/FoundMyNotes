27ca01b05b0f3ae1d7d4ea5badc8f7cf34c11b83
VennD0 <120389311+VennD0@users.noreply.github.com>
2023-05-21 13:39:35 +0800

    Reports

--- app/src/main/java/com/example/foundmynotes/Home.java
+++ app/src/main/java/com/example/foundmynotes/Home.java
@@ -8,22 +8,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Home extends AppCompatActivity {
-    Button btnReport;
+public class Home extends AppCompatActivity implements View.OnClickListener {
+    Button btnReport,Reports1;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+            Reports1 = findViewById(R.id.Reports1);
+             Reports1.setOnClickListener(this);
             btnReport= findViewById(R.id.btnReport);
-            btnReport.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent login = new Intent(getApplicationContext(), lostfoundreport.class);
-                    startActivity(login);
-                }
-            });
+            btnReport.setOnClickListener(this);
+        }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnReport:
+                Intent login = new Intent(getApplicationContext(), lostfoundreport.class);
+                startActivity(login);
+                break;
+            case R.id.Reports1:
+                Intent a = new Intent(getApplicationContext(), reports.class);
+                startActivity(a);
+                break;
         }
     }
+}
