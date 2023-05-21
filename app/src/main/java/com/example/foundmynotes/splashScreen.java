ee5a23b628f68f10a8413d2d43ed3a6c54b7c984
JohnChristianIdul <idulchristian@gmail.com>
2023-05-22 02:49:50 +0800

    Fixed the layout may 22 2023, 2:49 am

--- app/src/main/java/com/example/foundmynotes/splashScreen.java
+++ app/src/main/java/com/example/foundmynotes/splashScreen.java
@@ -1,14 +1,27 @@
 package com.example.foundmynotes;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Handler;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class splashScreen extends AppCompatActivity {
+
+    private static final int SPLASH_DELAY = 1000; // 1 second delay
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        // Delay for 1 second and then start the main activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splashScreen.this, Home.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DELAY);
     }
 }
