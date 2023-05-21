9b62527a2ec82d88f5185f2532298ed2e60dbec9
VennD0 <120389311+VennD0@users.noreply.github.com>
2023-05-21 13:31:37 +0800

    Reports

---
+++ app/src/main/java/com/example/foundmynotes/reports.java
@@ -0,0 +1,21 @@
+package com.example.foundmynotes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class reports extends AppCompatActivity {
+
+    Button btnReport;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.report);
+
+    }
+}
