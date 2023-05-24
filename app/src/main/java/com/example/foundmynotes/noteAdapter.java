d8d918d240136260954dab05babcbff4a63620a3
JohnChristianIdul <idulchristian@gmail.com>
2023-05-24 19:11:51 +0800

    Add Note Feature Done - IDUL

---
+++ app/src/main/java/com/example/foundmynotes/noteAdapter.java
@@ -0,0 +1,46 @@
+package com.example.foundmynotes;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.database.core.Context;
+
+public class noteAdapter extends FirestoreRecyclerAdapter <Note, noteAdapter.NoteViewHolder>{
+    Context context;
+
+    public noteAdapter(@NonNull FirestoreRecyclerOptions<Note> options, Context context) {
+        super(options);
+        this.context = context;
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull NoteViewHolder holder, int position, @NonNull Note note) {
+        holder.titleView.setText(note.title);
+        holder.contentView.setText(note.content);
+        holder.timestampView.setText(Utility.timestampToString(note.timestamp));
+    }
+
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+       View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_note_items, parent, false);
+        return new NoteViewHolder(view);
+    }
+
+    class NoteViewHolder extends RecyclerView.ViewHolder{
+        TextView titleView, contentView, timestampView;
+        public NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleView = itemView.findViewById(R.id.Title_Note);
+            contentView = itemView.findViewById(R.id.Content_Notes);
+            timestampView = itemView.findViewById(R.id.time_stamp);
+        }
+    }
+}
--- app/src/main/java/com/example/foundmynotes/notesPage.java
+++ app/src/main/java/com/example/foundmynotes/notesPage.java
@@ -1,21 +1,67 @@
 package com.example.foundmynotes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.FirestoreRegistrar;
+import com.google.firebase.firestore.Query;
+
 
 public class notesPage extends AppCompatActivity {
 
     FloatingActionButton btnAddNote;
+    RecyclerView recyclerView;
+    ImageButton btnMenu;
+    noteAdapter noteAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_page);
 
         btnAddNote = findViewById(R.id.btn_AddNote);
+        recyclerView = findViewById(R.id.recyclerView);
+        btnMenu = findViewById(R.id.btnMenuNotes);
+
         btnAddNote.setOnClickListener((v)->startActivity(new Intent(notesPage.this, noteDetailsActivity.class)));
+        btnMenu.setOnClickListener((v)->showMenu());
+        setupRecyclerView();
+    }
+
+    private void setupRecyclerView() {
+        Query query = Utility.getCollectionReferenceForNotes().orderBy("timestamp", Query.Direction.ASCENDING);
+        FirestoreRecyclerOptions<Note> options = new FirestoreRecyclerOptions.Builder<Note>()
+                .setQuery(query, Note.class).build();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        noteAdapter = new noteAdapter(options, this);
+        recyclerView.setAdapter(noteAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        noteAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        noteAdapter.stopListening();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        noteAdapter.notifyDataSetChanged();
+    }
+
+    void showMenu(){
+        // Display Menu
     }
 }
