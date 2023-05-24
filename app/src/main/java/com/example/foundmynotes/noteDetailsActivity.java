bc4cfd08d03077b21f8a1dcbdc9ba97d01f3bf6a
JohnChristianIdul <idulchristian@gmail.com>
2023-05-24 17:29:35 +0800

    Add New Note Feature - Idul

--- app/src/main/java/com/example/foundmynotes/noteDetailsActivity.java
+++ app/src/main/java/com/example/foundmynotes/noteDetailsActivity.java
@@ -3,12 +3,34 @@ package com.example.foundmynotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class noteDetailsActivity extends AppCompatActivity {
 
+    EditText titleEditText, contentEditText;
+    ImageButton btnSaveNote;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_details);
+
+        titleEditText = findViewById(R.id.notes_title_text);
+        contentEditText = findViewById(R.id.notes_content_text);
+        btnSaveNote = findViewById(R.id.btnSaveNote);
+
+        btnSaveNote.setOnClickListener( (v)->saveNote());
+
+    }
+
+    void saveNote(){
+        String note_Title = titleEditText.getText().toString();
+        String note_Content = contentEditText.getText().toString();
+
+        if(note_Title == null || note_Title.isEmpty()){
+            titleEditText.setError("Please input a title!");
+            return;
+        }
+
     }
 }
