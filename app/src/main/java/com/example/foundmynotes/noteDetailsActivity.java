5719d8bc29778452134e55079ebed2921bf0e05f
JohnChristianIdul <idulchristian@gmail.com>
2023-05-24 17:48:59 +0800

    Add New Note Feature 1 - Idul

--- app/src/main/java/com/example/foundmynotes/noteDetailsActivity.java
+++ app/src/main/java/com/example/foundmynotes/noteDetailsActivity.java
@@ -1,10 +1,16 @@
 package com.example.foundmynotes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageButton;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
 
 public class noteDetailsActivity extends AppCompatActivity {
 
@@ -32,5 +38,29 @@ public class noteDetailsActivity extends AppCompatActivity {
             return;
         }
 
+        Note note = new Note();
+        note.setTitle(note_Title);
+        note.setContent(note_Content);
+        note.setTimestamp(Timestamp.now());
+
+        saveNoteToFirebase(note);
+    }
+
+    private void saveNoteToFirebase(Note note) {
+        DocumentReference documentReference;
+        documentReference = Utility.getCollectionReferenceForNotes().document();
+
+        documentReference.set(note).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    //notes is added
+                    Utility.showToast(noteDetailsActivity.this, "Note added successfully!");
+                    finish();
+                }else{
+                    Utility.showToast(noteDetailsActivity.this, "Failed while adding the note!");
+                }
+            }
+        })
     }
 }
